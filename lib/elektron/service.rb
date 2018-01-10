f495f9a6aacd77cccb391226503844461b91fd35
Andreas Pfau <andreas.pfau@sap.com>
2018-01-10 10:59:44 +0100

    Revert "bugfix: recreate  http client if token has been expired and cannot be renewd"
    This reverts commit f9a042759a2ecb49898d2a918d93899401cc455e.

--- lib/elektron/service.rb
+++ lib/elektron/service.rb
@@ -179,14 +179,9 @@ module Elektron
     end
 
     def http_client(service_url)
-      token = begin
-                @auth_session.token
-              rescue Elektron::Errors::TokenExpired
-                # token has been expired
-                nil
-              end
+      token = @auth_session.token
       # caching
-      if token.nil? || @service_url != service_url || @token != token
+      if @service_url != service_url || @token != token
         options = @options.clone
         options[:headers]['X-Auth-Token'] = token
         @client = Elektron::HttpClient.new(service_url, options)
